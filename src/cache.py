0905d12fbca7051a659f7be260b60e719aac9bde
andrewfaircloth <148990622+andrewfaircloth@users.noreply.github.com>
2026-03-05 16:20:41 -0500

    Add README, sample data, and cache.py stub
    Add project README
    Add three example input/output placeholder files under data/
    Add src/cache.py containing stubbed FIFO, LRU, and OPTFF functions as a placeholder, implementations  still need to be completed.

---
+++ src/cache.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import io
+
+def FIFO(inputfile, outputfile){
+    number_of_misses = 0
+    return number_of_misses
+}
+    
+def LRU(inputfile, outputfile){
+    number_of_misses = 0
+    return number_of_misses
+}
+
+def OPTFF(inputfile, outputfile){
+    number_of_misses = 0
+    return number_of_misses
+}
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python cache.py <inputfile> <outputfile>")
+        sys.exit(1)
+
+    inputfile = sys.argv[1]
+    outputfile = sys.argv[2]
+
+    FIFO_misses = FIFO(inputfile, outputfile)
+    LRU_misses = LRU(inputfile, outputfile)
+    OPTFF_misses = OPTFF(inputfile, outputfile)
+
+    # Your program must output:
+    # FIFO  : <number_of_misses>
+    # LRU   : <number_of_misses>
+    # OPTFF : <number_of_misses>
+
+    # Use at least three nontrivial input files (each containing 50 or more requests)
+
+    print(f"FIFO  : {FIFO_misses}")
+    print(f"LRU   : {LRU_misses}")
+    print(f"OPTFF : {OPTFF_misses}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
