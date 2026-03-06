f0b57f4b4a0224f51f910c93ef480cdeec606330
Aln732 <149019274+Aln732@users.noreply.github.com>
2026-03-05 21:51:31 -0500

    Updating Cache.py with LRU

--- src/cache.py
+++ src/cache.py
@@ -2,20 +2,44 @@ import sys
 import os
 import io
 
-def FIFO(inputfile, outputfile){
-    number_of_misses = 0
-    return number_of_misses
-}
-    
-def LRU(inputfile, outputfile){
-    number_of_misses = 0
-    return number_of_misses
-}
 
-def OPTFF(inputfile, outputfile){
+def FIFO(inputfile, outputfile):
+    number_of_misses = 0
+    
+    
+    return number_of_misses
+
+    
+def LRU(inputfile, outputfile):
+    number_of_misses = 0
+    cache = []
+    file = open(inputfile, 'r')
+    params = file.readline().strip().split() 
+    data = file.read().strip().split()
+    #print(params, data)
+    for i in range(int(params[1])):
+        if data[i] not in cache:
+            #print("miss", data[i])
+            number_of_misses += 1
+            if len(cache) < int(params[0]):
+                cache.append(data[i])
+            else:
+                cache.pop(0)
+                cache.append(data[i])     
+        else:
+            #print("hit", data[i], "moved to back")
+            cache.remove(data[i])
+            cache.append(data[i])
+        #print(len(cache), cache)
+
+
+
+    return number_of_misses
+
+def OPTFF(inputfile, outputfile):
     number_of_misses = 0
     return number_of_misses
-}
+
 
 def main():
     if len(sys.argv) != 3:
@@ -39,6 +63,7 @@ def main():
     print(f"FIFO  : {FIFO_misses}")
     print(f"LRU   : {LRU_misses}")
     print(f"OPTFF : {OPTFF_misses}")
+    
 
     return 0
 
